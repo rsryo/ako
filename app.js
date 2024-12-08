44f78813f3588d99eb78523b72d5ff463c107b8e
rsryo <rs.ryoichi.09@gmail.com>
2024-12-08 17:08:54 +0900

    aa

--- app.js
+++ app.js
@@ -41,6 +41,6 @@ app.use('/prefix', prefixRouter);
 app.use('/', authRouter); // 認証関連のルートを追加
 
 // サーバー起動
-app.listen(port, () => {
-  console.log(`Server running on :${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
 });
