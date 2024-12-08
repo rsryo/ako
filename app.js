29453e3202e82e6a2dbc322d3b7ec8be1cdad602
rsryo <rs.ryoichi.09@gmail.com>
2024-12-08 20:55:28 +0900

    originを*に

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 // CORSの設定
 app.use(cors({
-  origin: 'https://akolibrary.org', // フロントエンドのURLを指定
+  origin: '*', // フロントエンドのURLを指定
   credentials: true, // クッキーなどの認証情報を含むリクエストを許可
 }));
 
