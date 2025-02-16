ae00a970bec5a10b6fdecb249796a898d30d9121
rsryo <rs.ryoichi.09@gmail.com>
2025-02-16 20:30:56 +0900

    bullの呼び出し方修正

--- routes/upload.js
+++ routes/upload.js
@@ -3,7 +3,6 @@ const multer = require('multer');
 const sharp = require('sharp');
 const { uploadFile } = require('../services/s3Operations');
 const { isAuthenticated } = require('./auth');
-const Queue = require('bull'); // このように修正
 
 const router = express.Router();
 
@@ -16,13 +15,6 @@ const upload = multer({
   },
 });
 
-// 非同期タスクを処理するためのバックグラウンドキュー
-const fileUploadQueue = new Queue('file-upload', 'redis://127.0.0.1:6379');
-
-fileUploadQueue.on('error', (err) => {
-  console.error('Error connecting to Redis:', err);
-});
-
 // 複数ファイルを受け取るように変更
 router.post('/', isAuthenticated, upload.array('files'), async (req, res) => {
   try {
@@ -33,46 +25,25 @@ router.post('/', isAuthenticated, upload.array('files'), async (req, res) => {
     const bucketName = 'slidelibrary';
     const prefix = req.query.prefix || '';
 
-    // 画像のアップロード処理をバックグラウンドで非同期に実行
-    const fileUploadPromises = req.files.map((file) => {
-      return fileUploadQueue.add({
-        fileBuffer: file.buffer,
-        fileName: file.originalname,
-        prefix: prefix,
-        bucketName: bucketName,
-      });
-    });
+    // 各ファイルをアップロード
+    const uploadPromises = req.files.map(async (file) => {
+      const key = `${prefix}${file.originalname}`;
 
-    // 非同期処理が全て完了したらレスポンスを返す
-    await Promise.all(fileUploadPromises);
-
-    res.status(200).json({ message: 'Files upload tasks started successfully.' });
-  } catch (error) {
-    console.error('Error in file upload route:', error);
-    res.status(500).json({ error: 'Error uploading files.' });
-  }
-});
-
-// バックグラウンドでの画像処理とアップロード処理
-fileUploadQueue.process(async (job, done) => {
-  try {
-    const { fileBuffer, fileName, prefix, bucketName } = job.data;
-
-    const key = `${prefix}${fileName}`;
-
-    // Sharpで画像をリサイズ・圧縮
-    const optimizedBuffer = await sharp(fileBuffer)
+      // Sharpで画像をリサイズ・圧縮
+      const optimizedBuffer = await sharp(file.buffer)
       .resize(800, 600, { fit: 'inside' }) // 最大800x600にリサイズ
       .jpeg({ quality: 70 }) // JPEG形式で圧縮率70%
       .toBuffer();
+      // 処理後の画像をS3にアップロード
+      return uploadFile(bucketName, key, optimizedBuffer, 'image/jpeg');
+    });
 
-    // 処理後の画像をS3にアップロード
-    await uploadFile(bucketName, key, optimizedBuffer, 'image/jpeg');
+    await Promise.all(uploadPromises);
 
-    done(); // タスク完了
+    res.status(200).json({ message: 'Files uploaded successfully' });
   } catch (error) {
-    console.error('Error processing file in queue:', error);
-    done(new Error('Error uploading file'));
+    console.error('Error uploading files:', error);
+    res.status(500).json({ error: 'Error uploading files.' });
   }
 });
 
